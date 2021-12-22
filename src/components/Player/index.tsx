abbbd7149cad6e7701485cb9eb01b3eca9d2aadc
Gerson Lamela <gersonlamela7@gmail.com>
2021-12-22 16:27:35 +0000

    NLW#5 - Workshop 03

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -5,7 +5,7 @@ export function Player(){
   return(
     <div className={styles.playerContainer}>
       <header>
-        <img src="./playing.svg" alt="Tocando agora"/>
+        <img src="/playing.svg" alt="Tocando agora"/>
         <strong>Tocando agora</strong>
       </header>
 
